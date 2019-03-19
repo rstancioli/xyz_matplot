2cab12decb9be6736b8627b5a80fbb8cb25db1b8
Rodrigo Stancioli <rodrigo_stancioli@yahoo.com.br>
2019-03-19 15:39:04 -0300

    PlotLattice class working.

--- plot.py
+++ plot.py
@@ -1,10 +1,13 @@
 import sys
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.collections import PatchCollection
 from matplotlib.animation import FuncAnimation
+
+print(matplotlib.get_backend())
 
 
 df_columns = ['type','x','y','z','colors','vx','vy','vz','energy']
@@ -89,55 +92,56 @@ class Frame():
         return colors
 
 
-def plot_lattice(frame, save=False, file_name='untitled_lattice.pdf'):
-    #fig = draw_plot(frame)
-    ax = draw_plot(frame)
-    if save:    
-        fig.savefig('./output/' + file_name + '.pdf', format='pdf')
-        print('Figure saved to file.')
-    fig.show()
+class PlotLattice():
 
-def draw_plot(frame):
-    #fig = plt.figure()
-    #ax = fig.subplots()
-    plt.cla()
-    ax.axis('off')
-    ax.set_aspect('equal')
-    for i, vertex_array in enumerate(frame.vertices):
-        if vertex_shapes[i] == 'circle':
-            shapes = [plt.Circle((xi,yi), radius=vertex_radius[i], linewidth=0, color=vertex_colors[i]) 
-                for xi,yi in zip(vertex_array[:,0], vertex_array[:,1])]
-        elif vertex_shapes[i] == 'diamond':
-            shapes = [patches.RegularPolygon((xi,yi), 4, radius=vertex_radius[i], linewidth=0, color=vertex_colors[i]) 
-                for xi,yi in zip(vertex_array[:,0], vertex_array[:,1])]
-        elif vertex_shapes[i] == 'square':
-            shapes = [patches.RegularPolygon((xi,yi), 4, radius=vertex_radius[i], orientation=.7854, linewidth=0, color=vertex_colors[i]) 
-                for xi,yi in zip(vertex_array[:,0], vertex_array[:,1])]
-        #for shape in shapes:
-        #    ax.add_artist(shape)
-        c = PatchCollection(shapes, match_original=True)
-        ax.add_collection(c)
-    for i, spin_array in enumerate(frame.spins):
-        colors = np.array(frame.colors[i], dtype=float)
-        ax.quiver(spin_array[:,0], spin_array[:,1], spin_array[:,2], spin_array[:,3], color=colors,
-            pivot='middle', units='x', width=.2, scale=1, headlength=3, headwidth=3,
-            headaxislength=2.5)
-    ax.set_xlim(left=0)
-    ax.set_ylim(bottom=0)
-    return ax
+    def __init__(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.subplots()
 
-def save_gif(collection, file_name='untitled_lattice.gif'):
-    #fig = draw_plot(collection[0])
-    #fig = plt.figure()
-    anim = FuncAnimation(fig, draw_plot, frames=collection)
-    anim.save('./output/' + file_name + '.gif', dpi=80, writer='imagemagick')
-    #plt.show()
+    def plot_lattice(self, frame, save=False, file_name='untitled_lattice.pdf'):
+        self.draw_plot(frame)
+        if save:    
+            self.fig.savefig('./output/' + file_name + '.pdf', format='pdf')
+            print('Figure saved to file.')
+        plt.show()
+
+    def draw_plot(self, frame):
+        plt.cla()
+        self.ax.axis('off')
+        self.ax.set_aspect('equal')
+        for i, vertex_array in enumerate(frame.vertices):
+            if vertex_shapes[i] == 'circle':
+                shapes = [plt.Circle((xi,yi), radius=vertex_radius[i], linewidth=0, color=vertex_colors[i]) 
+                    for xi,yi in zip(vertex_array[:,0], vertex_array[:,1])]
+            elif vertex_shapes[i] == 'diamond':
+                shapes = [patches.RegularPolygon((xi,yi), 4, radius=vertex_radius[i], linewidth=0, color=vertex_colors[i]) 
+                    for xi,yi in zip(vertex_array[:,0], vertex_array[:,1])]
+            elif vertex_shapes[i] == 'square':
+                shapes = [patches.RegularPolygon((xi,yi), 4, radius=vertex_radius[i], orientation=.7854, linewidth=0, color=vertex_colors[i]) 
+                    for xi,yi in zip(vertex_array[:,0], vertex_array[:,1])]
+            #for shape in shapes:
+            #    ax.add_artist(shape)
+            c = PatchCollection(shapes, match_original=True)
+            self.ax.add_collection(c)
+        for i, spin_array in enumerate(frame.spins):
+            colors = np.array(frame.colors[i], dtype=float)
+            self.ax.quiver(spin_array[:,0], spin_array[:,1], spin_array[:,2], spin_array[:,3], color=colors,
+                pivot='middle', units='x', width=.2, scale=1, headlength=3, headwidth=3,
+                headaxislength=2.5)
+        self.ax.set_xlim(left=0)
+        self.ax.set_ylim(bottom=0)
+        #return self.ax
+
+    def save_gif(self, collection, file_name='untitled_lattice.gif'):
+        #fig = draw_plot(collection[0])
+        #fig = plt.figure()
+        anim = FuncAnimation(self.fig, self.draw_plot, frames=collection)
+        anim.save('./output/' + file_name + '.gif', dpi=80, writer='imagemagick')
+        #plt.show()
 
 
 if __name__ == "__main__":
-    plt.clf()
-    fig = plt.figure()
-    ax = fig.subplots()
+    print(sys.argv)
     file_name = sys.argv[1]
     save = False
     gif = False
@@ -149,11 +153,12 @@ if __name__ == "__main__":
             gif = True
         output_file = sys.argv[3]
     new_collection = FrameCollection(file_name)
+    plt_lat = PlotLattice()
     if gif:
         print('gif')
-        save_gif(new_collection.collection, file_name=output_file)
+        plt_lat.save_gif(new_collection.collection, file_name=output_file)
     else:
         print('image')
-        plot_lattice(new_collection.collection[0], save=save, file_name=output_file)
+        plt_lat.plot_lattice(new_collection.collection[0], save=save, file_name=output_file)
     
     
